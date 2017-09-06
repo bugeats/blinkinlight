2fd3d491d29c3685a795130aaf57b38d8f7fbc6f
Chadwick Dahlquist <chadwick@bugeats.net>
2017-09-05 18:31:52 -0700

    App toggles lit state

--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
+const LIT_INTERVAL = 1000;
+
 import Lamp from './lamp';
 
-const App = () => {
-    return (
-      <div className='app'>
-        <h1>Blinkinlight Dev Harness</h1>
-        <div className='lamp-container'>
-          <Lamp/>
-        </div>
-      </div>
-    );
-};
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            lit: false
+        };
+
+        setInterval(() => {
+            this.setState({ lit: !this.state.lit });
+        }, LIT_INTERVAL);
+    }
+
+    render() {
+        return (
+          <div className='app'>
+            <h1>Blinkinlight Dev Harness</h1>
+            <div className='lamp-container'>
+              <Lamp lit={ this.state.lit }/>
+            </div>
+          </div>
+        );
+    }
+}
 
 export default App;
